1423574ff0fc9abc09894971035395c4af9dc958
ahmadlutfi <obzajd@gmail.com>
2022-01-15 03:22:12 -0500

    Commit: adds seeders, generators Reason: enforcing  a `Modular Approach`

--- main.jl
+++ main.jl
@@ -1,11 +1,18 @@
-"""
+#=
+
 (not an) AI package
 (but a) Statistics-based one
 
 Inspired by the Keras Project
-"""
+
+Ahmad Lutfi
+
+=#
+
+include("seeders")
 
 function seed()
+
     """
     function: generates a seed
     #TODO: can it be nothing?
@@ -14,6 +21,43 @@ function seed()
 # TODO:generate a seed
 return seed
 end
+
+function seed()
+    """
+    function: generates a seed
+    #TODO: can it be nothing?
+    """
+    seed = 0 #or
+# TODO:generate a seed
+
+return seed
+end
+
+function init(randomGenerator,seed ,μ=1,σ=0)
+    """
+    a functional function
+
+    inputs:
+
+    Mean μ  (fixed, #TODO: can it be a Variable?)
+    STD σ    (fixed , #TODO: can it be Variable?)
+    RNG seed
+    Function randomGenerator(seed)
+
+    outputs:
+
+
+    # TODO: Does the seed `remain` the same? can it be(come) changeable afterwards?
+    """
+    μ = μ
+    σ = σ
+    seed = seed
+
+    randomGenerator = randomGenerator(seed)
+
+    returns randomGenerator
+end
+
 
 function init(randomGenerator,seed ,μ=1,σ=0)
     """
@@ -32,7 +76,33 @@ function init(randomGenerator,seed ,μ=1,σ=0)
 
     model.randomGenerator = randomGenerator(seed)
 
+    returns randomGenerator
 end
 
+
+function init(randomGenerator,seed ,μ=1,σ=0)
+    """
+    inputs:
+
+    Mean μ [] (fixed, #TODO: can it be a Variable?)
+    STD σ    (fixed , #TODO: can it be Variable?)
+    RNG seed
+    Function randomGenerator(seed)
+
+    # TODO: Does the seed `remain` the same? can it be(come) changeable afterwards?
+    """
+
+
+    model.μ = μ
+    model.σ = σ
+    model.seed = seed
+
+    model.randomGenerator = randomGenerator(seed)
+
+    returns randomGenerator
+end
+
+
+#later
 z1 = zeros(3,3)
 z1!()
