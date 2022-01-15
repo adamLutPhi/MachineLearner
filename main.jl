b2e28231e3869983cd0e9d2e934f2e59ffb15edd
ahmadlutfi <obzajd@gmail.com>
2022-01-14 20:59:10 -0500

    commit: edits main

--- main.jl
+++ main.jl
@@ -1,3 +1,19 @@
 """
-inspired by keras project
+Inspired by the Keras Project
 """
+function seed()
+    seed = 0
+# TODO:generate a seed
+return seed
+end
+
+function init(randomGenerator,seed=nothing ,μ=1,σ=0)
+    self.μ = μ
+    self.σ = σ
+    self.seed = seed
+    self.randomGenerator = randomGenerator(seed)
+end
+
+
+z1 = zeros(3,3)
+z1!()
