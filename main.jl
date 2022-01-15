822dc37f5811f085354938dd9e408aadc23a280b
ahmadlutfi <obzajd@gmail.com>
2022-01-15 02:45:12 -0500

    commit - initial: adds Comments

--- main.jl
+++ main.jl
@@ -8,9 +8,21 @@ return seed
 end
 
 function init(randomGenerator,seed=nothing ,μ=1,σ=0)
-    self.μ = μ
-    self.σ = σ
-    self.seed = seed
+    """
+
+    inputs:
+
+    Mean μ  (fixed, can it be a Variable?)
+    STD σ    (fixed , can it be Variable?)
+    RNG seed
+    Function randomGenerator(seed)
+
+    Does the seed remains the same? can it be(come) changeable afterwards?
+    """
+
+    model.μ = μ
+    model.σ = σ
+    model .seed = seed
     self.randomGenerator = randomGenerator(seed)
 end
 
