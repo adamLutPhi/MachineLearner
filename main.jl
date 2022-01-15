4823b732bc6a2d35b629fe0c249c5b942be2e34b
ahmadlutfi <obzajd@gmail.com>
2022-01-15 02:46:44 -0500

    Commit: edits files

--- main.jl
+++ main.jl
@@ -1,31 +1,38 @@
 """
+(not an) AI package
+(but a) Statistics-based one
+
 Inspired by the Keras Project
 """
+
 function seed()
-    seed = 0
+    """
+    function: generates a seed
+    #TODO: can it be nothing?
+    """
+    seed = 0 #or
 # TODO:generate a seed
 return seed
 end
 
-function init(randomGenerator,seed=nothing ,μ=1,σ=0)
+function init(randomGenerator,seed ,μ=1,σ=0)
     """
-
     inputs:
 
-    Mean μ  (fixed, can it be a Variable?)
-    STD σ    (fixed , can it be Variable?)
+    Mean μ  (fixed, #TODO: can it be a Variable?)
+    STD σ    (fixed , #TODO: can it be Variable?)
     RNG seed
     Function randomGenerator(seed)
 
-    Does the seed remains the same? can it be(come) changeable afterwards?
+    # TODO: Does the seed `remain` the same? can it be(come) changeable afterwards?
     """
-
     model.μ = μ
     model.σ = σ
-    model .seed = seed
-    self.randomGenerator = randomGenerator(seed)
-end
+    model.seed = seed
 
+    model.randomGenerator = randomGenerator(seed)
+
+end
 
 z1 = zeros(3,3)
 z1!()
