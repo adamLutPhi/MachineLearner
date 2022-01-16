d7936968cac899f0c65f68581495756f02adb911
ahmadlutfi <obzajd@gmail.com>
2022-01-16 18:54:02 -0500

    commit: adds random function to generator

---
+++ generators/generator.jl
@@ -0,0 +1,25 @@
+import random
+module generator()
+
+#=
+references:
+
+-Random Int(64): 
+https://stackoverflow.com/questions/24326876/generating-a-random-integer-in-range-in-julia
+
+
+=#
+ function makerange(min=1, max)
+     """
+mimicing a caglag range(d) process
+starts from 1 - the origin (not 0?)
+
+     """
+
+
+function genericGenerator(sampleSize=300, genericModel=exp(-x))
+
+rand(1:n)
+end
+
+end
