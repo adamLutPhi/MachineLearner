3bc7026c06b3dcec9ab29c24d4b7f179f5622cff
ahmadlutfi <obzajd@gmail.com>
2022-01-16 19:24:37 -0500

    Commit: seperate into 2 files: - generator & randomness

--- generators/generator.jl
+++ generators/generator.jl
@@ -1,7 +1,9 @@
-import random
+using random
+include("randomness")
+
 module generator()
 
-export genericGenerator,
+export genericGenerator,randMatrix,randVector,randvalue
 #=
 references:
 
@@ -10,33 +12,16 @@ https://stackoverflow.com/questions/24326876/generating-a-random-integer-in-rang
 
 
 =#
-# function makerange(min=1, max)
 
-
-function randMatrix(a=10, b= 11, min=1, max=10)
-    return rand(min:max,a,b)
-end
-
-function randVector(min=1, max=10)
-    """
-    note: max must be Bounded
-    """
-
-    return rand(min:max)
-
-end
-
-
-function genericGenerator(sampleSize=300, genericModel=exp(-x))
+function genericGenerator(sampleSize=300,min=1::Int64,max=10::genericGenerator64, genericModel=exp(-x))
     """
     mimicing a caglag range(d) process
     ("continue à droite, limite à gauche")
     starts from 1 - the origin (not 0?)
-    Q. Should n always be an argument?
 
     """
-    n=10
-    randVector(1,n)
+    #initializes with a vector
+    randVector(min,max)
 end
 
 
