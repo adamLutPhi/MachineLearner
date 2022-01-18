31d7724f776dbf9d6a12b3f6056db39bdfa6354f
ahmadlutfi <obzajd@gmail.com>
2022-01-18 03:32:01 -0500

    commit: edits: -Distributions/genericDistribution -functions/smoothFunction -generators/generator

---
+++ functions/smoothFunction.jl
@@ -0,0 +1,22 @@
+
+export smooth, iscontinuous, isderivable
+
+function iscontinuous()
+
+end
+
+
+function isderivable()
+
+end
+
+
+function smooth(x)
+    """
+    A smooth function basis, infinitely:
+        1.continuous
+        2.derivable
+    """
+    return exp(x)
+
+end
