d02f474467b780a94a36f167309c36902a2733ef
ahmadlutfi <obzajd@gmail.com>
2022-01-19 06:49:27 -0500

    Commit: #major -adds CHANGELOG.md -adds RealAnalysis/bounds -adds generators/ziggurat stub

---
+++ functions/RealAnalysis/bounds.jl
@@ -0,0 +1,30 @@
+
+mutable struct range{a,b}
+    a=-Inf;b=Inf
+end
+
+function leastUpperBound(range)
+
+
+end
+
+function greatestLowerBound(range)
+
+end
+
+
+#---
+function sup(range)
+"""
+Supremum function
+"""
+
+end
+
+function inf(range)
+"""
+Infimum function
+"""
+
+
+end
---
+++ functions/SmoothFunctions/smoothFunction.jl
@@ -0,0 +1,22 @@
+
+export smooth, iscontinuous, isdifferentiable
+
+function iscontinuous()
+
+end
+
+
+function isdifferentiable()
+
+end
+
+
+function smooth(x)
+    """
+    A smooth function basis function, that is:
+        1.continuous
+        2.differentiablle
+    """
+    return exp(x)
+
+end
