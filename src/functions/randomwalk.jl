7d22b31116167b44be4e96ad06862187455e3935
ahmadlutfi <obzajd@gmail.com>
2022-01-25 09:15:21 -0500

    Commit: daily changes (2)

--- src/functions/randomwalk.jl
+++ src/functions/randomwalk.jl
@@ -2,19 +2,20 @@ using Distributions
 
 
 #--- Bernoulli
-```  
+
+
+function pdf(ntimes)
+"""  
 Returns a Bernoulli Probability Density Function (PDF) that is; 
 
 1. Discrete process  
 2. I.I.D
 
-```
-
-function pdf(ntimes)
+"""
   return Bernoulli(ntimes)
 end
 
-function cdf(pdf)
-    pdf()
+function cdf(pdf,x)
+    return sum(pdf(x)) #discrete sum 
 end
 #function #later
