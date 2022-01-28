a8d8b91678bdf1a913fdaaa50aef0a74a1d49837
Ahmad Lutfi <obzajd@gmail.com>
2022-01-28 18:48:57 -0500

    Commit: major retouching: constants, initializers

--- src/constants.jl
+++ src/constants.jl
@@ -8,33 +8,47 @@ global const -∞ = -Inf
 
 
 #Ε(x) = exp(x)
-#error function  ϵ (generic , cherry-picked smooth)
-global ϵ(x) =  exp(x)^-1
+#error function  ϵ (generic , cherry-picked, smooth)
+#global ϵ(x) =  exp(x)^-1
 
 #end
+end
 
 
 #--- Testing area #craziness allowed
+module accurateDigits
+#digits accuracy
+"""
+inputs:
+y: error function 
+returns number of accurate (at least) 
+TODO: How to test this test? 
 
-function accurateDigits()
-    #digits accuracy
-    """
-    TODO: How to test this test? 
+Yeilds an error ϵ with
+at least the number of Digits required (or better)
 
-    Yeilds an error ϵ with
-    at least the number of Digits required (or better)
+note: this is not a linear function
+it exponentially goes to z
 
-    note: this is not a linear function
-    it exponentially goes to z
+the logic:using the rule
+"if an error has a digit 0, that means the digit
+    (associated with this error) is accurate"
 
-    the logic:using the rule
-    "if an error has a digit 0, that means the digit
-        (associated with this error) is accurate"
+a homebrewed way, fabricated out of the spaciousness
+in the head - novel Creativity
+"""
+global ϵ(x) = exp(x)^-1
 
-    a homebrewed way, fabricated out of the spaciousness
-    in the head - Creativity
-    """
+#digi(n::Int64, x::Int64, y::Float64
+function digi(n = 6, x = 1, y = exp(x))
+    return ϵ(n * y)
+end
 
+#diffDigi(a::Int64, b::Int64)
+function diffDigi(a = 5, b = 6)
+    return digi(b) - digi(a)
+end
+#=
     digi = 6
     _6 = ϵ(digi * exp(1))
     ϵ(12)
@@ -42,8 +56,8 @@ function accurateDigits()
     digi1 = 5
     _5 = ϵ(digi1 * exp(1))
 
-    #for getting rid of non-linearity
-    #subtract between 2 consequtive numbers
+    #for getting rid of non-linearity (hopefully)
+    #subtract between 2 consequtive (non-linear) numbers
 
     diff = _5 - _6
     #=
@@ -52,14 +66,22 @@ function accurateDigits()
     =#
 
     _7 = ϵ(7 * exp(1))
+=#
 
+function maxEuclidDigi(a = 5, b = 6)
+    _a = digi(a)
+    _b = digi(b)
     #maximal euclidean distance
-    diff = (max(_5, _6) - min(_5, _6)) / 2
-    return diff
+    meanDiff = (max(_a, _b) - min(_a, _b)) / 2
+    return meanDiff
 end
+
 
 end 
 
+maxEuclidDigi(5,6)
+diffDigi(5,6)
+  @test diffDigi(5,6) == maxEuclidDigi(5,6) #there was an error during testing
 #=
 digi=6
 _6 = ϵ(digi*exp(1))
--- src/functions/Generators/generator.jl
+++ src/functions/Generators/generator.jl
@@ -6,86 +6,89 @@ references:
 using random, Test
 include("randomness.jl")# fixed: missing .jl !
 
-module generator()
+module generator
+()
 
-export genericGenerator,randMatrix,randVector,randvalue
+export genericGenerator, randMatrix, randVector, randvalue
 
- ```
-    mimicing a caglag, range(d) Process
-    ("continue à droite, limite à gauche")
-    starts from 1 - the origin (not 0?) #TODO:Check
+```
+   mimicing a caglag, range(d) Process
+   ("continue à droite, limite à gauche")
+   starts from 1 - the origin (not 0?) #TODO:Check
 
-    generates 
+   generates 
 
 
-    Input;
-    GenericModel: Must be a Smooth Function:
-    1. (Continuous on all Interval)
-    2. Differentiable on all Interval 
-    TODO: Add an Appropriate Differentiable module                                       
+   Input;
+   GenericModel: Must be a Smooth Function:
+   1. (Continuous on all Interval)
+   2. Differentiable on all Interval 
+   TODO: Add an Appropriate Differentiable module                                       
 
-    ```
+   ```
 
-        ```
-    PDF must play a role here
-    as 
-    TODO: use the argument 'genericModel' in a useful function  - does it need another loop?
-    ```
+```
+PDF must play a role here
+as 
+TODO: use the argument 'genericModel' in a useful function  - does it need another loop?
+```
 #working
 function genericGenerator(sampleSize = 300, _min = 1.0, _max = 10.0, genericModel = exp(-x))
 
-    
+
     x = linspace(_min, step, _max)
     y = zeros(length(x))
-   # z=nothing
-    a = nothing;b=nothing
-    for i  in enumerate(sampleSize) # <= sampleSize #generate sample
+    # z=nothing
+    a = nothing
+    b = nothing
+    for i in enumerate(sampleSize) # <= sampleSize #generate sample
         #initializes with a vector (TODO:Q.why a vector? isn't a point enough | this context?) - maybe that is the main problem
         b = randVector(_min, _max)
-       # c = copy(b)
+        # c = copy(b)
         a = a(a, b)
         j = x[i]
         y = genericModel(j)
-    
-    
+
+
     end
 
     x = linspace(_min, step, _max)
 
     y = zeros(length(x))
-  
+
 
     return a
 end
+
+#---range 
 
 function createRange(start, stop, length, step)
     return range(start, stop; length, step)
 end
 
 ```
-TODO: we need to figure out 
--the approach of doing the ziggurat
+TODO: we need to figure out:
 -which RNG to use 
 
 needs at least 2 loops
 ```
-function genSample(sampleSize=50::Int64,min=0.0,max=1.0)
- #range(min=0, stop=max; length=max, step=1)
-a= nothing
+function genSample(sampleSize = 50::Int64, min = 0.0, max = 1.0)
+    #range(min=0, stop=max; length=max, step=1)
+    a = nothing
     for i in enumerate(sampleSize) #range(start=min, stop=max; length=max-min, step=1)    # sampleSize +1 & i < max+1 : #generate sample
-       # for #TODO: 
+        # for #TODO: 
         #initializes with a vector (TODO:Q.why a vector? isn't a point enough | this context?) - maybe that is the main problem
-        a= (a, randVector(min,max))
+        a = (a, randVector(min, max))
 
     end
 end
 
- end
+end
 
 #---- testing area -- craziness allowed
 
-using Random, StableRNGs,Test
-lobound =  1
+using Random, StableRNGs, Test
+lobound = 1
 upbound = 128
 stepSize = 1 # TODO: which steepsize
 # 1 is the least Positive integer in Julia
@@ -112,27 +115,36 @@ generated from a specific seed,
 Rationale: guaranteeing that pseudorandom streams never change
 "Prohibits many algorithmic improvements"
 
-If you need to guarantee exact reproducibility of random data, it is advisable to simply save the data (e.g. as a supplementary attachment in a scientific publication). (You can also, of course, specify a particular Julia version and package manifest, especially if you require bit reproducibility.)
+If you need to guarantee `exact Reproducibility`` of random data, 
+it is advisable to simply save the data [me: to a text file]
+(e.g. as a supplementary attachment in a scientific publication). 
+(manifest: can also, of course, specify a particular Julia version and package manifest, 
+especially if you require bit reproducibility)
 
 Software tests that rely on specific "random" data should also generally either save the data, embed it into the test code, or
 use third-party packages like StableRNGs.jl.
- On the other hand, tests that should pass for most random data (e.g. testing A \ (A*x) ≈ x for a random matrix A = randn(n,n))
- can use an RNG, with a fixed seed, to ensure that simply running the test many times does not encounter a failure due to very improbable data (e.g. an extremely ill-conditioned matrix).
+On the other hand, tests that should pass for most random data (e.g. testing A \ (A*x) ≈ x for a random matrix A = randn(n,n))
+can use an RNG, with a fixed seed, to ensure that simply running the test many times does not encounter a failure due to very improbable data (e.g. an extremely ill-conditioned matrix).
 
-The statistical distribution from which random samples are drawn is guaranteed to be the same across any minor Julia releases.
+The Statistical Distribution from which random samples are drawn is guaranteed to be the same across any minor Julia releases.
 
 ```
-using Random
-seed = 1234; x1= rand(2)# 2 random variables
-rng()
-rng = MersenneTwister(seed); rand(rng, 2) == x1
+using Random, UUIDs
+seed = 1234;
+
+x1 = rand(2);# returns Uniformly, 2 random variables #rand([rng=GLOBAL_RNG], [S], [dims...])
+
+rng = MersenneTwister(seed); #create a MersenneTwister
+rand(rng, 2) == x1;#whats the pint storing x1 [reusability]
 reseed = Random.seed!(1234);
-Random.seed!(MersenneTwister(seed), seed) # (should be) a reproducible condition
-rand(rng, 2) == x1
+# Random.seed!(MersenneTwister(seed), seed) # (should be) a reproducible condition
+@test rand(rng, 2) == x1 #rand() regenerates everytime it's called 
 
-seed1 = rand(Random.seed!(rng), Bool) # not reproducible
+#rng()
+3points = rand(MersenneTwister(1234), 3)
+seed1 = rand(Random.seed!(MersenneTwister(1234)), Bool) # not reproducible
 
-seed2 = rand(Random.seed!(rng), Bool) # (not) reproducible
+seed2 = rand(Random.seed!(MersenneTwister(1234)), Bool) # (not) reproducible
 
 seed3 = rand(MersenneTwister(), Bool) # not rng has no seed. Infer reproducible either
 
@@ -156,24 +168,24 @@ distinct type from StableRNG in any upcoming minor
 rng = StableRNG(123)
 A = randn(rng, 10, 10)
 num1 = randn(rng, 1)
-num2=randn(rng,1) # instead of randn(10, 10)
+num2 = randn(rng, 1) # instead of randn(10, 10)
 @test inv(inv(A)) ≈ A
 @test inv(randn(A))
 #---range
-range1(a=lobound,b=upbound) = lobound:stepSize:length(upbound)
+range1(a = lobound, b = upbound) = lobound:stepSize:length(upbound)
 Random.Sampler
 rand(range1)
 
 
-range2(a=lobound,b=upbound) = (lobound in Base.range(lobound,upbound;(upbound-lobound),stepSize))
+range2(a = lobound, b = upbound) = (lobound in Base.range(lobound, upbound; (upbound - lobound), stepSize))
 
 ##r range1(2,8)
 
-rand([rng=GLOBAL_RNG],[r])
+rand([rng = GLOBAL_RNG], [r])
 
 #--- Base.://
 Function
-//(num=3, den=5)
+//(num = 3, den = 5)
 """
 Divide two integers or rational numbers, giving a Rational result.
 """
@@ -183,7 +195,7 @@ Divide two integers or rational numbers, giving a Rational result.
 #---range
 
 
-function range_step_stop_length(a,step, len::Integer)
+function range_step_stop_length(a, step, len::Integer)
     start = a - step * (len - oneunit(len))
     if start isa Signed
         # overflow in recomputing length from stop is okay
@@ -193,19 +205,19 @@ function range_step_stop_length(a,step, len::Integer)
 end
 
 for i in enumerate(1000)
-	@test(range_step_stop_length(1,1,100))
+    @test(range_step_stop_length(1, 1, 100))
 
 end
 # Stop and length as the only argument
-function range_stop_length(start,a, len::Integer)
-        # overflow in recomputing length from stop is okay
+function range_stop_length(start, a, len::Integer)
+    # overflow in recomputing length from stop is okay
 
     return StepRangeLen{typeof(start),typeof(start),typeof(step)}(start, step, len)
 end
 # Stop and length as the only argument
 
 #a=0;b=100;len = length(b - a)
- 
+
 function range_stop_length(a = 0, b = 100)
     # Start and length as the only argument
 
@@ -216,10 +228,10 @@ end
 #--- convertions
 
 function array2vector1(T::Any)
-return irr(vec(Float64.(T)))
-rationalize(vec(Float64.(T)))
+    return irr(vec(Float64.(T)))
+    rationalize(vec(Float64.(T)))
 end
 function array2vector2(T::Any)
-return
-Float64.(vcat(T[1], vec(T[2])))
+    return
+    Float64.(vcat(T[1], vec(T[2])))
 end
