01c8ec3457be2039853901acd1c074c54ee57374
Ahmad Lutfi <obzajd@gmail.com>
2022-01-31 09:34:10 -0500

    Elaborates on Topic

--- abstract/topic.jl
+++ abstract/topic.jl
@@ -4,5 +4,10 @@ topic (or subject) -whicever fits best
 struct topic <: idea 
 
     function topic() end 
+    function topic(id,name,meta )
+        id = id 
+        name = name
+        meta = meta
+    end 
     function debate(Set{concept}, Set{topic}, Set{issue})
 end
