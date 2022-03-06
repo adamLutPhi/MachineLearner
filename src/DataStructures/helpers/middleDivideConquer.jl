3d52163d4e73c3ea9845e0ed5de38cae3285394e
Ahmad Lutfi <obzajd@gmail.com>
2022-03-05 21:26:46 -0500

    Update: function functionality

--- src/DataStructures/helpers/middleDivideConquer.jl
+++ src/DataStructures/helpers/middleDivideConquer.jl
@@ -1,3 +1,18 @@
+#=
+20 named functions ( 19 without counting the mit function)  (counting helper functions) 
+10 helper functions 
+
+Divide & Conquer 
+
+if middlle 
+    - is even?:Yes -> has 1 middle 
+    -if not, there's a fractionalMid, need to approximate index to get below & above 
+else 
+ go left 
+
+ go right 
+
+=#
 import Base: @propagate_inbounds, @inbounds
 import BenchmarkTools: @btime, @time, @benchmark
 UnexpMsg = "ERROR: unexpected input:  please check input arguments , then try again  "
@@ -7,20 +22,23 @@ positiveMsg = "ERROR: Only Positive input arguments are allowed - please check i
 
 @benchmark euclidDist(10000, 1000000) =#
 
+@propagate_inbounds isPositive(num) = num > 0 ? true : false
+
+
 @propagate_inbounds euclidDist(a, b) = abs(a + b) # Range (min … max):  0.001 ns … 0.100 ns
 #ambiguous here : even is calling middle #Warning 
 
-@propagate_inbounds isEven(st = 1, ed = 10) = euclidDist(st, ed) % 2 == 0 ? true : false
+@propagate_inbounds isEven(st = 1, ed = 10) =
+    isPositive(num) && euclidDist(st, ed) % 2 == 0 ? true : false
 
 
-@propagate_inbounds isEven(num) = num % 2 == 0 ? true : false #q. do we check only middle, or also the  total length ?  (depends )
+@propagate_inbounds isEven(num) = isPositive(num) && num % 2 == 0 ? true : false #q. do we check only middle, or also the  total length ?  (depends )
 
 
-@propagate_inbounds ϟ(a, b) =
-    a - b >= 0 || b - a >= 0 ? max(a, b) - min(a, b) : println(positiveMsg) #  abs(a + b)
+ϟ(a, b) = (a - b > 0) ⊻ (b - a > 0) ? max(a, b) - min(a, b) : println(positiveMsg) #  abs(a - b)
 
 @propagate_inbounds function doCompare(st = 1, ed = 2, a = [2, 1, 3, 4])
-    
+
     # Base.@propagate_inbounds 
     @inbounds if a[st] > a[ed]
         #Base.@propagate_inbounds  
@@ -54,6 +72,7 @@ end
     end
 end
 
+
 """ Assumes there is a rational series of numbers from point a to point b """
 @propagate_inbounds function buildAboveSoBelow(a, below, above, b) #Checked 
     if a >= 0 && below >= 0 && above >= 0 && b >= 0
@@ -66,12 +85,14 @@ end
         println(positiveMsg)
     end
 end
-"""with a vector provided, """
+
+
+"""with a vector provided, making things easier with Comparing, on the spot"""
 @propagate_inbounds function buildAboveSoBelow(a, below, above, b, vec) #Checked 
     if a >= 0 && below >= 0 && above >= 0 && b >= 0
         #doCompare function
-        doCompare(a, b,vec) #a,b are aompared 
-        doCompare( above, below, vec)
+        doCompare(a, b, vec) #a,b are Compared 
+        doCompare(above, below, vec)
         q = []
         @inbounds push!(q, makeRange(a, below))
         @inbounds push!(q, makeRange(below, above))
@@ -81,25 +102,26 @@ end
         println(positiveMsg)
     end
 end
-
+#---------------------------------------
 
 ranges = buildAboveSoBelow(1, 5, 6, 11) #exhausts mid ranges[2] = 6 , 3 element, left, mid, right # still: left, Right 
 
 
-#---- middle ------------
+#---- middle ----------- 
 @propagate_inbounds function middle(a, b) #use this 
     q = []
     # mid = middle(a, b) # function calls itself! 
     mid = euclidDist(a, b) #enforce \upkoppa 
     cond = isEven(mid)
 
-    if cond # got mid, mid +1 #start of next range 
+    if cond #isEven,  got mid, mid +1 #start of next range 
         q = buildRangeAroundPoint(a, mid, b)
         # push!(ranges, buildRangeAroundPoint(a, mid, b)) # returns [a, mid] , [mid+1, b]
 
-    elseif !cond # got 2 midpoints float, in middle , floor(below), ceil(above)
+    elseif !cond #!isEven got a fraction, estimated to  2 midpoint Indicies, around middle: below=floor(mid), above = ceil(mid)
         mid = mid / 2  # get  updated middle (fractional) 
         above = Int(ceil(mid))
+
         below = Int(floor(mid))
         q = buildAboveSoBelow(a, below, above, b)
         # push!(ranges, buildAboveSoBelow(a, below, above, b)) # [a, below] , [above, b]
@@ -109,39 +131,71 @@ ranges = buildAboveSoBelow(1, 5, 6, 11) #exhausts mid ranges[2] = 6 , 3 element,
     end
     return q
 end
+middle(1,5) #errourneous  2.5 3, 4  
+#------------------------------------------
+#e.g. Example 
+
 arr = [1, 22, 34, 44, 55]
+#=
+for i in 1:length(arr)
+    println(popfirst!(arr))   
+end 
+=#
 actualSize = length(arr) + 1  #length = size - 1 #5 
-if actualSize == 1
-    return
+
+#= 
+ if actualSize == 1
+#    return
 elseif actualSize >= 1 # still values to choose from 
-    #euclidD = actualSize
-    if !isEven(actualSize) # false 
+#    evinity = isEven(actualSize)
+    #euclidDist(arr[])
+    if !evinity #!isEven(actualSize) # false  
+
+    #end
+    elseif evinity
+
     end
-end
+=#
+isEven(actualSize)
 #--- 
 
 q = []
 #-------------------------
-function isNotDivide(range)
-    cond = abs(range1[2] - range1[1])  # upkoppa #condition Not Divide 
-    response = nothing
+""" important function for divideConquer: the terminal condition where the distance of ranges become 1  """
+function isToNotDivide(range::UnitRange)
+    cond = abs(length(range)[2] - (range)[1])  # upkoppa #condition Not Divide 
+    # response = nothing
     if cond == 1 # check current range # if 1 (nothing more to explore )
-        #finish this range, get out 
-        #final
+        #To Not Divide this range, get out of program 
         return true
 
-    elseif cond > 1 # there are still ranges in-between  (to explore)
-        #TODO 
+    else#if cond > 1 # there are still ranges in-between  (to explore)
+        # To Divide  
         return false
+        #else 
     end
+
 end
-"""completes the infinite dragon, at the end there is only 1"""
+
+length(6:10) - length(3:7) === length(6:10) - length(5:9)
+length(6:10) - length(6:10)
+#they  not equal 
+#--------------
+6:10-3:6 #6:10 - 3:10  = 6:7:6 #enigma #can it be go around 6 7 times (the only explination that makes sense)
+[3]
+[2]
+(1:2)[1]
+isToNotDivide(1:2)
+isToNotDivide(1:4)
+
+"""calls divideConquer - as  at the end there is only One""" # completes the infinite dragon
 function calldivideConquer(arr, range)
     divideConquer(arr, range[1], length(range[1]))
     divideConquer(arr, range[2], length(range[2]))
 
 end
-#the absurd call of divide
+#the absurd call of the  divide (divine)
+
 #--------------------------------------------/
 
 
@@ -157,6 +211,7 @@ if !isEven(actualSize) # false # 5 there is a fractional middle with ceil & abov
 
     q = buildAboveSoBelow(a, below, above, b)
 end
+#----------------------------------------------
 
 arr = [1, 2, 3, 4, 5]
 #  length(arr)
@@ -167,14 +222,67 @@ above = Int(ceil(fractionalMid)) # 3
 below = Int(floor(fractionalMid)) # 2
 
 q = buildAboveSoBelow(a, below, above, b)
+#= [1, 2]
+ [2, 3]
+ [3, 4, 5]=#
+l = length(q)
+count = 1 #starting from 1
+#=
+0 -> 3 (3)
+1-> 4 (3)
+this correctly counts 1 -> 3 (sumRanges 3+1 = 4 but max is 3 )
+=#
+l = length(q[2]) # 2 
+array = []; # [firstvalue]
+count = 1
+temp = deepcopy(q[2])
+
+typeof(q[2][1]) #Vector #int64
+
+res = q[2] # pop!(q[2]) 
+typeof(q[2])
+
+a
+for i = 1:l #enumerate(q) # in 0:l#4 Always!    #enumerate(q) also works fine as Base.Iterator.Enumerate 
+    #count would be 3 = length(q) A lways 
+    #count<=l ? count += 1 : break; # count ==3 max
+    #array[i] =i 
+    push!(array, i)
+    item = pop!(q[2]) #pop!(collection) -> item # Remove the last item in collection and return it.
+    #i<=l  ? count+= 1 : break; 
+    #count+=1 # counts 4 ( size = length(q) + 1 ) #either need to count -1 
+
+end # will count 4 
+#count-=1 #learnd heuristic 
+
+array
+count -= 1
+
+#count = 0->3 0 index or 1 ->4 1-index 
+ℸ = []
+#deepcopy(ℸ,)
+
+for i = 1:l # 1-> 3 in this case, loop would alway count 3 = intended max  
+    #    count+=1 #either this (naive) or 
+    #count<=l ? count += 1 : break; # count ==3 max #sophisicated 
+    println(i)# infer i is most accurate 
+    #insert!([4, 2, 1,3], 4, 3)
+    push!(ℸ, i)
+    #    popfirst!(q[2]) #2 , 3
+end
+ℸ
+q[2]
+
+
 #q = buildAboveSoBelow(a, below, above, b)
 #doCompare()
-q
+
 doCompare(below, above, arr)
 #middle(ar)
 
 q = middle(1, 10) # true
-length(q) # if length == 3 : Right middle, Left situation #examine each on its own 
+typeof(q)
+l = length(q) # if length == 3 : Right middle, Left situation #examine each on its own 
 #if subrange == 1 return 
 length(q[1])
 #middle(q[1])
@@ -190,7 +298,7 @@ ranges = []
 @btime resMid = buildRangeAroundPoint(1, 5, 10) # 104.920 ns - 105.297 ns  (4 allocations: 320 bytes)
 
 
-#=UncommentMe
+#=UncommentMe #seems an alternative is already present 
 """a middle function stub without array/vector input feed- the Best? - unbounded """
 @propagate_inbounds function middle(a, b)
     mid = middle(a, b)
@@ -213,7 +321,8 @@ end
 #--- testing ------
 
 #---test buildMiddle ------
-ranges = buildMiddle(1, 2, 4) # error brings up numbers not in the list (as if its reading off a theoretical array found only in the head )
+#ERROR:
+# ranges = buildMiddle(1, 2, 4) # error brings up numbers not in the list (as if its reading off a theoretical array found only in the head )
 
 typeof(ranges)
 #range = popfirst!(ranges)
@@ -221,40 +330,21 @@ typeof(ranges)
 
 #--- test middle(a,b) ---
 
-#---test buildAboveSoBelow----
+#--- test buildAboveSoBelow----
 #--- test - easy : 
 res = buildAboveSoBelow(1, 3, 5, 10) # a <below < above < b 
+
 #fine 
 res = buildAboveSoBelow(1, -1, 5, 10) #TODO: check input arguments are only positive 
+typeof(res) #nothing, rather than an errourneous outcome
+
 #up until negative - 1 : range is omitted, returns an empty Int64[] range , else return other positive ranges # fine
 #Warning: results in an Unbalanced Ranges 
-#--- 
+#---end
+
 #---
 #check n/2 is a whole number
 #--- Left Half 
-
-"""divideConquer """
-
-#choosing a,b is choosing arr , this is a mean, not a goal 
-  divideConquer(arr, range[1], length(range[1]))
-@propagate_inbounds function divideConquer(arr = [1, 2, 3, 4], a = 1, b = length(arr))  ## arr is an initial value, while the goal would be , 1  length(other size of bound to walk on  ) a code that never walks..
-    count = 1
-    mid = middle(a, b) # first Domino
-    #check valid range 
-    cond = mid - count >= 0 ? true : false
-    q = []
-
-    #if cond
-    if a[mid] < a[mid-1] # only look at left half 1 . . . n/2 − − − 1 to look for peak
-        pushfirst!(q, goleft(arr, a, mid))
-
-    elseif a[mid] < a[mid+1] # only look at right half n/2 + 1 . . . n to look for peak
-        pushfirst!(q, goright(a))
-
-    else # a[mid] =?= a[mid+1]
-        pushfirst!(q, mid) # the peak!
-    end
-end
 
 function goleft(arr = [1, 2, 3, 4], a = 1, b = length(arr))
     computeRange!(arr, a, b)
@@ -264,9 +354,39 @@ function goright(arr = [1, 2, 3, 4], a = 1, b = length(arr))  #mid + 1, b = leng
     computeRange!(arr, a, b)
     isEven(length(arr)) #either even or not 
 end
+#--------------------------------------------------------------------------
+"""divideConquer """
+
+# creditL mit 6.006 
+#choosing a,b is choosing arr , this is a mean, not a goal 
+# divideConquer(arr, range[1], length(range[1])) #check 
+@propagate_inbounds function divideConquer(arr = [1, 2, 3, 4], a = arr[1], b = length(arr))  ## arr is an initial value, while the goal would be , 1  length(other size of bound to walk on  ) a code that never walks..
+    count = 1
+    mid = middle(a, b) # first Domino #this presumes middle returns 1 middle # immature 
+    #check valid range 
+    cond = ϟ(mid, count) > 0 ? true : false #= mid - count >= 0=#
+    q = []
+
+    if cond
+        count += 1
+        if a[mid] < a[mid-1] # only look at left half 1 . . . n/2 − − − 1 to look for peak
+            pushfirst!(q, goleft(arr, a, mid))
+
+        elseif a[mid] < a[mid+1] # only look at right half n/2 + 1 . . . n to look for peak
+            pushfirst!(q, goright(a))
+
+        else # a[mid] =?= a[mid+1]
+            pushfirst!(q, mid) # the peak!
+        end
+    end
+end
+#no errors 
+
 
 #---test ------
 arr = [1, 2, 3, 4]
+divideConquer(arr, arr[1], length(arr))
+
 length(arr)
 #middle(st=1, ed =4)
 mid = middle(1, 4) # ambiguous function  # StackOverflowError:
@@ -301,7 +421,7 @@ function middle(a, b) # working
     end
     return q #condition, check, above, below
 end
-ϟ(a, b) = abs(a + b) # floating-point division euclideanDist(a, b) / 2 * 1.0 # freely allowing floats, to be ceiled & floored 
+
 #---test --- 
 mid = 2.5
 above = Int(ceil(mid))
@@ -357,7 +477,7 @@ res = -1
 # 1+3/2 = 2 
 #yeilds 1:2 , 2:3 
 #check dist 
-abs(1 - 2) == 1
+
 abs(2 - 3) == 1
 # final comparison 
 #compare current ranges with their value , do comparison when required  
@@ -370,7 +490,7 @@ a[1]
 @inbounds if cond == true
     res = (1 + 3) // 2
     pushfirst!(q, Int(res))
-     elseif cond == false
+elseif cond == false
     res = 1 + 3 / 2
     above = ceil(res)
     pushfirst!(q, Int(above))
@@ -421,10 +541,13 @@ collect(range2)
 
 range1[1]
 cond = abs(range1[2] - range1[1]) # 4. check distance #the thing: 3 -4 == 1 
-response = isNotDivide(range1)
-calldivideConquer(arr, range1)
+response(range1) = isToNotDivide(range1) # unsued Important function #TODO: use it 
 
-#Halt
+if !response(range1) # this means it will only be called when true 
+    calldivideConquer(arr, range1)
+else #
+end
+#Halt 
 
 
 calldivideConquer(arr, range1)
@@ -462,8 +585,9 @@ a, b = comp([1, 2, 3, 4], st, ed) #3. compare  # returns values of these  at ind
 range1 = collect(a:b)
 #returns new range 2:3 # vectorized version 
 
-
 #"""assumes lower indicies have lower values, swap otherwise """ #Redundant
+#----------------
+
 @propagate_inbounds function comp(a = [1, 2, 3, 4], st = 1, ed = 2)
     if st < ed
         #@boundscheck if a[st] > a[ed]
@@ -474,4 +598,4 @@ range1 = collect(a:b)
     elseif st > ed # ed smaller can work with that 
         a[ed], a[st] = doCompare(a, ed, st)
     end
-end
+end #LoadError: syntax: incomplete: "if" at c:\Users\adamus\.git\very Deep\DeepLearner\DeepLearner\src\DataStructures\helpers\middleDivideConquer.jl:500 requires end
