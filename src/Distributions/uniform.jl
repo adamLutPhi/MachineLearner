282200568e62f550d102cd93f3a79a9040b329e6
Ahmad Lutfi <obzajd@gmail.com>
2022-09-08 10:28:00 -0400

    Update uniform.jl

--- src/Distributions/uniform.jl
+++ src/Distributions/uniform.jl
@@ -8,10 +8,11 @@ module uniform
 
     ```
     """
+#TODO: Calculate the uniform pdf
     function pdf(x, range)
         return pdf(x, range)
     end
-
+#TODO: check sum equals approx. to the cdf 
     function cdf(x, range) #TODO: approx Integral of pdf(x)
         return sum(pdf(x, range)) #sum of all points #assumes all po
     end
